bb0da9fc52f1ae9aede7be931db59d5cf7506b4f
julianatonu <julianatonu@gmail.com>
2025-04-12 23:49:10 +0600

    add

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -28,6 +28,7 @@ const Navbar = () => {
             <div className="hidden sm:flex items-center gap-8">
                 <NavLink to='/'>Home</NavLink>
                 <NavLink to='/products'>All product</NavLink>
+                <NavLink to='/contact'>My orders</NavLink>
                 <NavLink to='/contact'>Contact</NavLink>
 
 
