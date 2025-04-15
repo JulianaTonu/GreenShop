08017c5fb953f6309766077b2c8a03ea36dcb6a0
julianatonu <julianatonu@gmail.com>
2025-04-15 12:00:17 +0600

    add

---
+++ client/src/components/MainBanner.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import main_banner_bg from '../assets/main_banner_bg.png';
+import main_banner_bg_sm from '../assets/main_banner_bg_sm.png';
+
+
+
+const MainBanner = () => {
+    return (
+        <div className='relative'>
+            <img src={main_banner_bg} alt="banner" className='w-full hidden md:block' />
+            <img src={main_banner_bg_sm} alt="banner" className='w-full  md:hidden' />
+        </div>
+    );
+};
+
+export default MainBanner;
