216fee0487d276fe273b53cab18cd26a88b0ab2b
julianatonu <julianatonu@gmail.com>
2025-05-01 15:10:13 +0600

    databaseConnected

---
+++ server/configs/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        mongoose.connection.on('connected', () => {
+            console.log("Database Connected")
+        });
+        console.log("🔍 ENV MONGODB_URL:",process.env.MONGODB_URL);
+        await mongoose.connect(`${process.env.MONGODB_URL}/greenshop`)
+    } catch (error) {
+        console.error("Database connection error:", error.message);
+    }
+}
+
+export default connectDB;
--- server/server.js
+++ server/server.js
@@ -1,17 +1,23 @@
 import cookieParser from 'cookie-parser';
 import express from 'express';
 import cors from 'cors';
+import connectDB from './configs/db.js';
+
+import dotenv from 'dotenv';
+dotenv.config()
 
 const app = express();
 const port = process.env.PORT || 4000;
 
+await connectDB()
+
 //allow multipleOrigins
-const allowedOrigins =['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173']
 
 //middleware
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({origin:allowedOrigins,Credential:true}))
+app.use(cors({ origin: allowedOrigins, credentials: true }))
 
 app.get('/', (req, res) =>
     res.send("API is working")
