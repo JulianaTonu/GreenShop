e27d4211cc5275026dfacec3c8a51aae50c698c7
julianatonu <julianatonu@gmail.com>
2025-05-03 13:06:32 +0600

    userController

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -7,7 +7,7 @@ export const register = async (req, res) => {
     try {
         const { name, email, password } = req.body;
         if (!name || !email || !password) {
-            return res.json({ success: false, message: "Missing Details" })
+            return res.json({ success: false, message: "Missing Details,Please provide all required fields." })
         }
         const existingUser = await User.findOne({ email })
         if (existingUser)
@@ -15,12 +15,25 @@ export const register = async (req, res) => {
 
         const hashedPassword = await bcrypt.hash(password, 10)
         const user = await User.create({ name, email, password: hashedPassword })
-        const token = jwt.sign({ id: user._id},process.env.JWT_SECRET,{expiresIn:'7d'})
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' })
 
-        res.cookie('token',token, {
-            httpOnly:true, //Prevent Javascript to access cookie
-           
+        res.cookie('token', token, {
+            httpOnly: true, //Prevent Javascript to access cookie
+            secure: process.env.NODE_ENV === 'production', //use secure cookiees in production
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict', //CSRF protection 
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+
+        })
+        return res.json({
+            success:true,
+            message:'User already exists',
+        user:{
+            email: user.email,
+            name: user.name
+        }
         })
     } catch (error) {
+        console.error("Register Error:", error);
+        return res.status(500).json({ success: false, message: "Server error. Please try again later." });
     }
 }
