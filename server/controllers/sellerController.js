ab582a7f57f594b6b7bab81069a8bbe60956813e
julianatonu <julianatonu@gmail.com>
2025-05-04 15:43:44 +0600

    sellerLogout

--- server/controllers/sellerController.js
+++ server/controllers/sellerController.js
@@ -38,3 +38,17 @@ export const isSellerAuth = async (req, res) => {
 }
 
 
+// Logout Seller : /api/seller/logout
+export const sellerLogout = async (req, res) => {
+    try {
+        res.clearCookie('sellerToken', {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        })
+        res.json({ success: true, message: "Logged Out" })
+    } catch (error) {
+        console.log(error.message)
+        res.json({ success: false, message: error.message })
+    }
+}
